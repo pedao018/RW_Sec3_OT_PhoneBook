2540e9e6e1df344c37b9a71aad597a5ec45ea35a
HAIND <HAIND haind@hqsoft.com.vn>
2021-11-14 22:11:16 +0700

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/rw_sec3_ot_phonebook/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.raywenderlich.rw_sec3_ot_phonebook.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.raywenderlich.rw_sec3_ot_phonebook.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
